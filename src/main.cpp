d3e796a6d7c787ba9cb8555fa8343be50359cd5a
paul randazzo <paulrandazzo.42@gmail.com>
2018-09-19 11:50:36 +0200

    update de lib et main

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,18 @@
 
 int main(){
 
-std::cout<< "Hello" << std::endl;
-calcul* calcul1 = new calcul(6, 5);
+int a;
+int b;
+std::cout << "entrer le premier nombre"<<std::endl;
+std::cin >> a;
+std::cout<< "entrer le second nombre" << std::endl;
+std::cin >> b;
+
+
+std::cout<< "La somme est" << std::endl;
+
+
+calcul* calcul1 = new calcul(a, b);
 
 
 
