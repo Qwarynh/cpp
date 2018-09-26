dbb5ba5c5b792076cc33777b8d95c3164ca52b04
Euphoria <eupho.42@gmail.com>
2018-09-26 11:28:56 +0200

    mise à jour

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include "lib.h"
-#include "ecole.h"
+//#include "lib.h"
+//#include "ecole.h"
 //#include <stdlib.h>
 
 int main(){
@@ -18,11 +18,11 @@ std::cout<< "La somme est" << std::endl;
 
 calcul* calcul1 = new calcul;
 calcul calcul2;
-ecole ecole1;
+//ecole ecole1;
 
 calcul1->somme(a,b);
 
-std::cout << "la date de creation de l'ecole est " << ecole1.date()<<std::endl;
+//std::cout << "la date de creation de l'ecole est " << ecole1.date()<<std::endl;
 
 
 calcul2.multiplication(a, b);
